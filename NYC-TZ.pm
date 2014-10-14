ecb7fb6e036e45bf5151beba1c803606cd95fe9d
Chris Jepeway <jepeway@blasted-heath.com>
2014-10-13 22:29:01 -0400

    - finish renaming NYT-TZ to NYC-TZ

--- NYC-TZ.pm
+++ NYC-TZ.pm
@@ -1,6 +1,6 @@
 use TimeZone;
 
-class NYT-TZ does TimeZone { 
+class NYC-TZ does TimeZone { 
 	method new() { self.bless(name => 'America/New_York'); }
 	method utc-offset-in-seconds(:(Instant $when? = now)) returns Int { 4 * 60 * 60 }
 	method abbreviation(:(Instant $when? = now)) returns Str { 'EDT' }
