9d54abe1b86df5dace5073925f69cd720f28fb25
Chris Jepeway <jepeway@blasted-heath.com>
2014-10-13 22:26:07 -0400

    - finish splitting NYT-TZ out of old TimeZone class

--- NYT-TZ.pm
+++ NYT-TZ.pm
@@ -1,12 +1,4 @@
-role TimeZone {
-	has Str $.name;
-
-	method utc-offset-in-seconds(:(Instant $when? = now)) returns Int { ... }
-	method abbreviation(:(Instant $when? = now)) { ... }
-
-	method Numeric() { self.utc-offset-in-seconds() }
-	method Str() { self.abbreviation() }
-}
+use TimeZone;
 
 class NYT-TZ does TimeZone { 
 	method new() { self.bless(name => 'America/New_York'); }
--- TimeZone.pm6
+++ TimeZone.pm6
@@ -7,9 +7,3 @@ role TimeZone {
 	method Numeric() { self.utc-offset-in-seconds() }
 	method Str() { self.abbreviation() }
 }
-
-class NYT-TZ does TimeZone { 
-	method new() { self.bless(name => 'America/New_York'); }
-	method utc-offset-in-seconds(:(Instant $when? = now)) returns Int { 4 * 60 * 60 }
-	method abbreviation(:(Instant $when? = now)) returns Str { 'EDT' }
-}
