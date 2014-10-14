8aa7752886aaba4f20c8f7fa38f607cf8a091598
Chris Jepeway <jepeway@blasted-heath.com>
2014-10-13 22:56:28 -0400

    - start adding Olson-based impl

---
+++ Olson-TZ.pm
@@ -0,0 +1,7 @@
+use TimeZone;
+
+class NYC-TZ does TimeZone { 
+	method new() { self.bless(name => 'America/New_York'); }
+	method utc-offset-in-seconds(:(Instant $when? = now)) returns Int { 4 * 60 * 60 }
+	method abbreviation(:(Instant $when? = now)) returns Str { 'EDT' }
+}
