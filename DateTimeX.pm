4b8514e5f5d196a69c8423c7cef6e23edbb5463a
Chris Jepeway <jepeway@blasted-heath.com>
2014-11-19 00:08:04 -0500

    - add DateTimeX.in-timezone & tests; these fail

--- DateTimeX.pm
+++ DateTimeX.pm
@@ -1,1 +1,15 @@
-class DateTimeX is DateTime;
+use TimeZone;
+my Int $i;
+class DateTimeX is DateTime {
+    multi method in-timezone(Int $offset) returns DateTimeX {
+	$i++;
+	say "$i: ", Backtrace.new.concise;
+	return DateTimeX if $i > 3;
+	return self.later(seconds => $offset)
+    }
+
+    multi method in-timezone(TimeZone $tz) returns DateTimeX {
+	say 2;
+	return self.later(seconds => $tz.utc-offset-in-seconds(self.posix));
+    }
+}
