d357a75de1c05df2211905c0b9884ba99393f026
Chris Jepeway <jepeway@blasted-heath.com>
2014-11-19 10:19:39 -0500

    - fix DateTimeX so it won't recurse infinitely

--- DateTimeX.pm
+++ DateTimeX.pm
@@ -2,14 +2,14 @@ use TimeZone;
 my Int $i;
 class DateTimeX is DateTime {
     multi method in-timezone(Int $offset) returns DateTimeX {
-	$i++;
-	say "$i: ", Backtrace.new.concise;
-	return DateTimeX if $i > 3;
-	return self.later(seconds => $offset)
+	my DateTime $dt .= new(self.posix, :timezone($offset));
+	return self.clone-without-validating(:year($dt.year), :month($dt.month), :day($dt.day),
+					     :hour($dt.hour), :second($dt.second),
+					     :timezone($dt.timezone),
+					     :formatter($dt.formatter));
     }
 
     multi method in-timezone(TimeZone $tz) returns DateTimeX {
-	say 2;
-	return self.later(seconds => $tz.utc-offset-in-seconds(self.posix));
+	self.in-timezone($tz.utc-offset-in-seconds(self.posix));
     }
 }
