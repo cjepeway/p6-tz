bb1033da8c3fc5082b317eab134ec26dc1ec22ff
Chris Jepeway <jepeway@blasted-heath.com>
2014-11-19 19:18:34 -0500

    - added t/04-datetime tests to t/05-datetimex - fixed botched plural forms of DateTimeX attrs in t/05-datetimex - improved a few is() msgs - added new() methods to DateTimeX that (are meant to)   support a TimeZone argument; these introduce infinite   recursion into in-timezone() somehow

--- DateTimeX.pm
+++ DateTimeX.pm
@@ -1,7 +1,11 @@
 use TimeZone;
-my Int $i;
+use UTC-TZ;
+
 class DateTimeX is DateTime {
     multi method in-timezone(Int $offset) returns DateTimeX {
+	say '>1';
+	die 'too deep'
+		if Backtrace.new.concise.gist.comb(/\n/).elems > 10;
 	my DateTime $dt .= new(self.posix, :timezone($offset));
 	return self.clone-without-validating(:year($dt.year), :month($dt.month), :day($dt.day),
 					     :hour($dt.hour), :second($dt.second),
@@ -10,6 +14,24 @@ class DateTimeX is DateTime {
     }
 
     multi method in-timezone(TimeZone $tz) returns DateTimeX {
+	say '>2';
 	self.in-timezone($tz.utc-offset-in-seconds(self.posix));
+    }
+
+    multi method new(Int :$year, Int :$month = 1, Int :$day = 1,
+		     Int :$hour = 0, Int :$minute = 0, :$second = 0,
+		     TimeZone :$timezone,
+		     :&formatter = &DateTime::default-formatter) {
+	say '>3';
+	my DateTimeX $dt .= new($year, $month, $day, $hour, $minute, $second);
+	self.new($dt.earlier($timezone.utc-offset-in-seconds($dt.posix)).posix, $timezone);
+    }
+
+    multi method new(TimeZone :$timezone = UTC-TZ.new, *%_) {
+	say '>4';
+	%_.say;
+	my DateTime $dt .= new(|%_);
+	$dt.perl.say;
+	self.new($dt.earlier(seconds => $timezone.utc-offset-in-seconds($dt.posix)).posix, :$timezone);
     }
 }
