5366b14a623534be14c7d871b04645acbb54f336
Chris Jepeway <jepeway@blasted-heath.com>
2015-04-20 08:01:47 -0400

    - reverted calls to mktime() - forced $.timezone to be a TimeZone - changed/added in-timezone methods to preserve TimeZone diff --git a/DateTimeX.pm b/DateTimeX.pm index c33b8bc..0f76383 100644 --- a/DateTimeX.pm +++ b/DateTimeX.pm @@ -2,25 +2,43 @@ use TimeZone;  use UTC-TZ;
     class DateTimeX is DateTime {
    +    has TimeZone $.timezone;
    +
         method posix() {
    -	self.timezone.mktime(self);
    +	callwith(True);
         }
    
    -    multi method in-timezone(Int $offset) returns DateTimeX {
    +#
    +#	really, this method should fail(); if it were ever implemented, we'd know
    +#	we'd created a DateTimeX that didn't have a TimeZone
    +#
    +#    multi method in-timezone(Int $offset) returns DateTimeX {
    +#	say ">0\n", Backtrace.new.concise;
    +#	die 'too deep'
    +#		if Backtrace.new.concise.gist.comb(/\n/).elems > 10;
    +#	my DateTime $dt .= new(self.posix, :timezone($offset));
    +#	return self.clone-without-validating(:year($dt.year), :month($dt.month), :day($dt.day),
    +#					     :hour($dt.hour), :second($dt.second),
    +#					     :timezone($dt.timezone),
    +#					     :formatter($dt.formatter));
    +#    }
    +
    +    # stolen from DateTime, augmented to keep a TimeZone around
    +    multi method new(Instant:D $i, TimeZone :$timezone=UTC-TZ.new, :&formatter=&default-formatter) {
     	say ">1\n", Backtrace.new.concise;
    -	die 'too deep'
    -		if Backtrace.new.concise.gist.comb(/\n/).elems > 10;
    -	my DateTime $dt .= new(self.posix, :timezone($offset));
    -	return self.clone-without-validating(:year($dt.year), :month($dt.month), :day($dt.day),
    -					     :hour($dt.hour), :second($dt.second),
    -					     :timezone($dt.timezone),
    -					     :formatter($dt.formatter));
    +        my ($p, $leap-second) = $i.to-posix;
    +        my $dt = self.new: floor($p - $leap-second).Int, :&formatter, :$timezone;
    +        $dt.clone(second => ($dt.second + $p % 1 + $leap-second)
    +            ).in-timezone($timezone);
         }
    
         multi method in-timezone(TimeZone $tz) {
     	say ">2\n", Backtrace.new.concise;
    -	my DateTimeX $dtx .= new(self.posix, :timezone($tz.utc-offset-in-seconds(self.posix)));
    -	$dtx.clone-without-validating(:timezone($tz));
    +	my DateTime $dt .= new(self.posix, :timezone($tz.utc-offset-in-seconds(self.posix)));
    +	return self.clone-without-validating(:year($dt.year), :month($dt.month), :day($dt.day),
    +					     :hour($dt.hour), :second($dt.second),
    +					     :timezone($tz),
    +					     :formatter($dt.formatter));
         }
    
         multi method new(TimeZone :$timezone = UTC-TZ.new, *%_) {

--- DateTimeX.pm
+++ DateTimeX.pm
@@ -2,25 +2,43 @@ use TimeZone;
 use UTC-TZ;
 
 class DateTimeX is DateTime {
+    has TimeZone $.timezone;
+
     method posix() {
-	self.timezone.mktime(self);
+	callwith(True);
     }
 
-    multi method in-timezone(Int $offset) returns DateTimeX {
+#
+#	really, this method should fail(); if it were ever implemented, we'd know
+#	we'd created a DateTimeX that didn't have a TimeZone
+#
+#    multi method in-timezone(Int $offset) returns DateTimeX {
+#	say ">0\n", Backtrace.new.concise;
+#	die 'too deep'
+#		if Backtrace.new.concise.gist.comb(/\n/).elems > 10;
+#	my DateTime $dt .= new(self.posix, :timezone($offset));
+#	return self.clone-without-validating(:year($dt.year), :month($dt.month), :day($dt.day),
+#					     :hour($dt.hour), :second($dt.second),
+#					     :timezone($dt.timezone),
+#					     :formatter($dt.formatter));
+#    }
+
+    # stolen from DateTime, augmented to keep a TimeZone around
+    multi method new(Instant:D $i, TimeZone :$timezone=UTC-TZ.new, :&formatter=&default-formatter) {
 	say ">1\n", Backtrace.new.concise;
-	die 'too deep'
-		if Backtrace.new.concise.gist.comb(/\n/).elems > 10;
-	my DateTime $dt .= new(self.posix, :timezone($offset));
-	return self.clone-without-validating(:year($dt.year), :month($dt.month), :day($dt.day),
-					     :hour($dt.hour), :second($dt.second),
-					     :timezone($dt.timezone),
-					     :formatter($dt.formatter));
+        my ($p, $leap-second) = $i.to-posix;
+        my $dt = self.new: floor($p - $leap-second).Int, :&formatter, :$timezone;
+        $dt.clone(second => ($dt.second + $p % 1 + $leap-second)
+            ).in-timezone($timezone);
     }
 
     multi method in-timezone(TimeZone $tz) {
 	say ">2\n", Backtrace.new.concise;
-	my DateTimeX $dtx .= new(self.posix, :timezone($tz.utc-offset-in-seconds(self.posix)));
-	$dtx.clone-without-validating(:timezone($tz));
+	my DateTime $dt .= new(self.posix, :timezone($tz.utc-offset-in-seconds(self.posix)));
+	return self.clone-without-validating(:year($dt.year), :month($dt.month), :day($dt.day),
+					     :hour($dt.hour), :second($dt.second),
+					     :timezone($tz),
+					     :formatter($dt.formatter));
     }
 
     multi method new(TimeZone :$timezone = UTC-TZ.new, *%_) {
