ac42396de056f746f51a51e3baf6c38bd5f2dc22
Chris Jepeway <jepeway@blasted-heath.com>
2015-04-25 14:57:19 -0400

    - implemented remaining multi's in TimeZone.pm

--- UTC-TZ.pm
+++ UTC-TZ.pm
@@ -2,6 +2,8 @@ use TimeZone;
 
 class UTC-TZ is TimeZone {
 	method new() { self.bless(name => 'UTC'); }
-	multi method utc-offset-in-seconds(Int $when? = 0) returns Int { 0 }
-	multi method abbreviation(Int $when? = time) returns Str { 'UTC' }
+	multi method utc-offset-in-seconds(Int $when) returns Int { 0 }
+	multi method utc-offset-in-seconds(DateTime) returns Int { 0 }
+	multi method abbreviation(Int $when) returns Str { 'UTC' }
+	multi method abbreviation(DateTime $when) returns Str { 'UTC' }
 }
