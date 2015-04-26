6499668667ecb34ddf1f2db66e683e0a7c0182a0
Chris Jepeway <jepeway@blasted-heath.com>
2015-04-26 19:45:35 -0400

    - sprinkle debugs - comment out other debugs - try to force $.timezone to be defined via :D - make .in-timezone(Int) die with non-zero arg, as allowing - delegate to DateTimeX objects instead of DateTime ones   non-zero would slice off the TimeZonedness of $.timezone

--- DateTimeX.pm
+++ DateTimeX.pm
@@ -2,40 +2,38 @@ use TimeZone;
 use UTC-TZ;
 
 class DateTimeX is DateTime {
-    has TimeZone $.timezone;
+    has TimeZone:D $.timezone;
 
     method posix() {
+	say ">0\n", Backtrace.new.concise;
+	self.perl.say;
+	$.timezone.perl.say;
 	$.timezone.WHAT.say;
 	$.timezone.say;
 	callwith(True) - $.timezone.utc-offset-in-seconds(self);
     }
 
-#
-#	really, this method should fail(); if it were ever implemented, we'd know
-#	we'd created a DateTimeX that didn't have a TimeZone
-#
-#    multi method in-timezone(Int $offset) returns DateTimeX {
-#	say ">0\n", Backtrace.new.concise;
-#	die 'too deep'
-#		if Backtrace.new.concise.gist.comb(/\n/).elems > 10;
-#	my DateTime $dt .= new(self.posix, :timezone($offset));
-#	return self.clone-without-validating(:year($dt.year), :month($dt.month), :day($dt.day),
-#					     :hour($dt.hour), :second($dt.second),
-#					     :timezone($dt.timezone),
-#					     :formatter($dt.formatter));
-#    }
+    multi method in-timezone(Int $offset) returns DateTimeX {
+	die "slicing TimeZone off " ~ self
+		unless $offset == 0;
+	return self.in-timezone(UTC-TZ.new);
+    }
 
     # stolen from DateTime, augmented to keep a TimeZone around
     multi method new(Instant:D $i, TimeZone :$timezone=UTC-TZ.new, :&formatter=&default-formatter) {
 	say ">1\n", Backtrace.new.concise;
+	$timezone.perl.say;
         my ($p, $leap-second) = $i.to-posix;
-        my $dt = self.new: floor($p - $leap-second).Int, :&formatter, :$timezone;
-        $dt.clone(second => ($dt.second + $p % 1 + $leap-second)
+        my DateTimeX $dtx = self.new: floor($p - $leap-second).Int, :&formatter, :$timezone;
+        $dtx.clone(second => ($dtx.second + $p % 1 + $leap-second)
             ).in-timezone($timezone);
     }
 
     multi method in-timezone(TimeZone:D $tz) {
 	say ">2\n", Backtrace.new.concise;
+	self.perl.say;
+	$tz.perl.say;
+	$tz.utc-offset-in-seconds(self).say;
 	my DateTime $dt .= new(self.posix, :timezone($tz.utc-offset-in-seconds(self)));
 	return self.clone-without-validating(:year($dt.year), :month($dt.month), :day($dt.day),
 					     :hour($dt.hour), :second($dt.second),
@@ -43,15 +41,16 @@ class DateTimeX is DateTime {
 					     :formatter($dt.formatter));
     }
 
+
     multi method new(TimeZone :$timezone = UTC-TZ.new, *%_) {
-	say ">3\n", Backtrace.new.concise;
-	$timezone.perl.say;
-	%_.say;
+	#say ">3\n", Backtrace.new.concise;
+	#$timezone.perl.say;
+	#%_.say;
 	my $me = self.bless(:$timezone, |%_);
-	$me.perl.say;
+	#$me.perl.say;
 	$me;
 	#my $dtx = callwith(|%_);
-	#$dtx.perl.say;
+	##$dtx.perl.say;
 	#$dtx.timezone = $timezone;
 	#$dtx;
     }
