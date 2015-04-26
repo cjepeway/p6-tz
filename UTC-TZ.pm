9bfeaca1eb67136f4769bb9129608b6f22e789f7
Chris Jepeway <jepeway@blasted-heath.com>
2015-04-25 20:20:39 -0400

    - added formal param to utc-offset-in-seconds(DateTime)

--- UTC-TZ.pm
+++ UTC-TZ.pm
@@ -3,7 +3,7 @@ use TimeZone;
 class UTC-TZ is TimeZone {
 	method new() { self.bless(name => 'UTC'); }
 	multi method utc-offset-in-seconds(Int $when) returns Int { 0 }
-	multi method utc-offset-in-seconds(DateTime) returns Int { 0 }
+	multi method utc-offset-in-seconds(DateTime $when) returns Int { 0 }
 	multi method abbreviation(Int $when) returns Str { 'UTC' }
 	multi method abbreviation(DateTime $when) returns Str { 'UTC' }
 }
