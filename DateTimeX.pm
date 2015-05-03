b680c4443da9e3bcb13fd9a57e3cf288ec9da1da
Chris Jepeway <jepeway@blasted-heath.com>
2015-05-03 10:20:30 -0400

    - commented out debugs

--- DateTimeX.pm
+++ DateTimeX.pm
@@ -5,11 +5,13 @@ class DateTimeX is DateTime {
     has TimeZone:D $.timezone;
 
     method posix() {
-	say ">0\n", Backtrace.new.concise;
-	self.perl.say;
-	$.timezone.perl.say;
-	$.timezone.WHAT.say;
-	$.timezone.say;
+	#say ">0\n", Backtrace.new.concise;
+	#self.perl.say;
+	#$.timezone.perl.say;
+	#$.timezone.WHAT.say;
+	#$.timezone.utc-offset-in-seconds(self).say;
+	#say 'abbr: ', $.timezone.abbreviation(self);
+	#$.timezone.say;
 	callwith(True) - $.timezone.utc-offset-in-seconds(self);
     }
 
@@ -21,8 +23,8 @@ class DateTimeX is DateTime {
 
     # stolen from DateTime, augmented to keep a TimeZone around
     multi method new(Instant:D $i, TimeZone :$timezone=UTC-TZ.new, :&formatter=&default-formatter) {
-	say ">1\n", Backtrace.new.concise;
-	$timezone.perl.say;
+	#say ">1\n", Backtrace.new.concise;
+	#$timezone.perl.say;
         my ($p, $leap-second) = $i.to-posix;
         my DateTimeX $dtx = self.new: floor($p - $leap-second).Int, :&formatter, :$timezone;
         $dtx.clone(second => ($dtx.second + $p % 1 + $leap-second)
@@ -30,10 +32,10 @@ class DateTimeX is DateTime {
     }
 
     multi method in-timezone(TimeZone:D $tz) {
-	say ">2\n", Backtrace.new.concise;
-	self.perl.say;
-	$tz.perl.say;
-	$tz.utc-offset-in-seconds(self).say;
+	#say ">2\n", Backtrace.new.concise;
+	#self.perl.say;
+	#$tz.perl.say;
+	#$tz.utc-offset-in-seconds(self).say;
 	my DateTime $dt .= new(self.posix, :timezone($tz.utc-offset-in-seconds(self)));
 	return self.clone-without-validating(:year($dt.year), :month($dt.month), :day($dt.day),
 					     :hour($dt.hour), :second($dt.second),
@@ -43,14 +45,14 @@ class DateTimeX is DateTime {
 
 
     multi method new(TimeZone :$timezone = UTC-TZ.new, *%_) {
-	#say ">3\n", Backtrace.new.concise;
-	#$timezone.perl.say;
-	#%_.say;
+	##say ">3\n", Backtrace.new.concise;
+	##$timezone.perl.say;
+	##%_.say;
 	my $me = self.bless(:$timezone, |%_);
-	#$me.perl.say;
+	##$me.perl.say;
 	$me;
 	#my $dtx = callwith(|%_);
-	##$dtx.perl.say;
+	###$dtx.perl.say;
 	#$dtx.timezone = $timezone;
 	#$dtx;
     }
