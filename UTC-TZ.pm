bf56145ed1327deb0be84b0c2f0334dcc7df4737
Chris Jepeway <jepeway@blasted-heath.com>
2015-05-06 07:57:40 -0400

    - added missing method keyword

--- UTC-TZ.pm
+++ UTC-TZ.pm
@@ -1,7 +1,7 @@
 use Basic-TZ;
 
 class UTC-TZ does Basic-TZ {
-	new() { self.bless(name => 'UTC'); }
+	method new() { self.bless(name => 'UTC'); }
 	multi method utc-offset-in-seconds(Int $when = time) returns Int { 0 }
 	multi method utc-offset-in-seconds(DateTime $when) returns Int { 0 }
 	method utc() returns TimeZone { return self; }
